cd200d11a0a7a9247828a61a879e1cfbc8bdc3bf
wwmairs <wwmairs@gmail.com>
2017-09-05 13:06:41 -0400

    debugging still

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var proxy = require('express-http-proxy');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use('/', proxy('wwmairs.com:82/'));
 
-app.listen(80);
+app.listen(app.get('port'));
