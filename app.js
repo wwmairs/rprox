5695d3c74e2ee72d042bec4d9fe69d7cc13f1c67
wwmairs <wwmairs@gmail.com>
2017-09-05 13:07:55 -0400

    removing domain

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var proxy = require('express-http-proxy');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', proxy('wwmairs.com:82/'));
+app.use('/', proxy('209.6.43.162:82/'));
 
 app.listen(app.get('port'));
