f0a8aec311d2a50c3fbd1811fe18ba258893aaf4
wwmairs <wwmairs@gmail.com>
2017-09-05 12:56:52 -0400

    debug

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var proxy = require('express-http-proxy');
 
-app.use('/', proxy('wwmairs.com:82'));
+app.use('/', proxy('wwmairs.com:82/'));
 
 app.listen(80);
